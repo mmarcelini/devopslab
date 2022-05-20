a6c2a72a106cf24896244e37dc3c3f275a5dd364
mmarcelini <miltonmarcelini@gmail.com>
2022-05-20 12:25:23 -0300

    teste conflito

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def pagina_inicial():
-    return "DevOps na Veia Fiap !!!"
+    return " DevOps na Veia Fiap !!! "
 
 if __name__ == '__main__':
     app.run()
